c881166c7d3a3a6efd34421dd8dab9dfb3af97c8
phks20 <phks20@inf.ufpr.br>
2025-04-01 11:58:39 -0300

    [modify] update root-finding methods to include calculation type parameter

--- t0/ZeroFuncao.c
+++ t0/ZeroFuncao.c
@@ -7,7 +7,6 @@
 
 #include "utils.h"
 
-
 // Converte double para int64 preservando os bits
 int64_t double_to_int64(double d) {
     int64_t i;
@@ -26,38 +25,35 @@ int64_t calcula_diferenca_ulp(double a, double b) {
     return diff;
 }
 
-
 // Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
-real_t newtonRaphson(Polinomio p, real_t x0, int criterioParada, int *it,
-                     real_t *raiz) {
+real_t newtonRaphson(Polinomio p, real_t x0, int criterioParada, int *it, real_t *raiz, int tipoCalculo) {
     int parada = 0;
 
-    real_t derivada = 0.1, ultimaDerivada, ultimaRaiz, novoX = x0, erro = 0.0, px;
-    
+    real_t derivada = 0.0, ultimaDerivada = 0.0, erro = 0.0, px = 0.0;
+
     while (!parada) {
         (*it)++;
-        ultimaRaiz = *raiz;
         ultimaDerivada = derivada;
-        px = 0;
-        calcPolinomio_rapido(p, *raiz, &px, &derivada);
-        // printf("raiz candidata: %lf\n", *raiz);
-        
-        
-        if(derivada <= DBL_EPSILON) {
-            derivada = ultimaDerivada;
-            // printf("Derivada muito pequena, usando a derivada anterior: %lf\n", derivada);
+        if(tipoCalculo == 0) {
+            calcPolinomio_rapido(p, x0, &px, &derivada);
+        } else if(tipoCalculo == 1) {
+            calcPolinomio_lento(p, x0, &px, &derivada);
         }
-        
-        x0 = *raiz;
-        *raiz = *raiz - (px / derivada);  // calcula a raiz candidata
-        erro = fabs(*raiz - x0) / *raiz * 100;
+
+        if (fabs(derivada) <= DBL_EPSILON) {
+            derivada = ultimaDerivada;
+        }
+
+        x0 = *raiz; // recebe a ultima raiz candidata
+        *raiz = *raiz - (px / derivada);  // calcula a nova raiz candidata
+        erro = fabs(*raiz - x0) / *raiz * 100; // calcula o erro relativo
 
         if (*it >= MAXIT) {
             parada = 1;
             // printf("Número máximo de iterações atingido\n");
             return erro;  // iterações excedidas, retorna o erro
         }
-        if ((criterioParada == 1) && (*it > 2) && (fabs(ultimaRaiz - (*raiz)) < 0.000001)) {  // criterio 1 - erro absoluto entre xk e xk-1
+        if ((criterioParada == 1) && (*it > 2) && (fabs(x0 - (*raiz)) < 0.000001)) {  // criterio 1 - erro absoluto entre xk e xk-1
             parada = 1;
             // printf("Erro absoluto entre xk (%lf) e xk-1 (%lf): %lf\n", ultimaRaiz, *raiz, fabs(ultimaRaiz - (*raiz)));
             return erro;
@@ -67,12 +63,10 @@ real_t newtonRaphson(Polinomio p, real_t x0, int criterioParada, int *it,
             // printf("Valor de p(x) é zero: %lf\n", fabs(calcRaizCandidata));
             return erro;
         }
-        if (criterioParada == 3 && (*it > 2) && calcula_diferenca_ulp(ultimaRaiz, *raiz) <= 2) {  // criterio 3 - ULP's entre xk e xk-1 <= 2
+        if (criterioParada == 3 && (*it > 2) && calcula_diferenca_ulp(x0, *raiz) <= 2) {  // criterio 3 - ULP's entre xk e xk-1 <= 2
             parada = 1;
             return erro;
         }
-
-        
     }
 }
 
@@ -86,7 +80,7 @@ real_t raiz - valor da raiz que foi encontrado
 
 // Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
 */
-real_t bisseccao(Polinomio p, real_t a, real_t b, int criterioParada, int *it, real_t *raiz) {
+real_t bisseccao(Polinomio p, real_t a, real_t b, int criterioParada, int *it, real_t *raiz, int tipoCalculo) {
     int parada = 0;
     real_t erro = 0;
     real_t calcRaizCandidata, calcA, calcB;
@@ -96,9 +90,15 @@ real_t bisseccao(Polinomio p, real_t a, real_t b, int criterioParada, int *it, r
         *raiz = (a + b) / 2;  // calcula a raiz candidata, o ponto medio do intervalo
         erro = (fabs(ultimaRaiz - *raiz) / ultimaRaiz) * 100;
         // printf("testando a raiz candidata: %lf\n", *raiz);
-        calcPolinomio_rapido(p, a, &calcA, NULL);
-        calcPolinomio_rapido(p, b, &calcB, NULL);
-        calcPolinomio_rapido(p, *raiz, &calcRaizCandidata, NULL);
+        if (tipoCalculo == 0) {
+            calcPolinomio_rapido(p, a, &calcA, NULL);
+            calcPolinomio_rapido(p, b, &calcB, NULL);
+            calcPolinomio_rapido(p, *raiz, &calcRaizCandidata, NULL);
+        } else if (tipoCalculo == 1) {
+            calcPolinomio_lento(p, a, &calcA, NULL);
+            calcPolinomio_lento(p, b, &calcB, NULL);
+            calcPolinomio_lento(p, *raiz, &calcRaizCandidata, NULL);
+        }
 
         if (calcA * calcRaizCandidata < 0) {
             // printf("b recebe raizCandidata, pois a raiz está no intervalo [a, raiz]\n");
@@ -164,6 +164,8 @@ real_t *px - valor de P(X) calculado
 real_t *dpx - valor da derivada de P(x) calculado, i.e, f'(x)
 */
 void calcPolinomio_lento(Polinomio p, real_t x, real_t *px, real_t *dpx) {
+    *px = 0;
+    if (dpx != NULL) *dpx = 0;
     for (int i = p.grau; i >= 0; i--) {
         *px += p.p[i] * pow(x, i);  // O(n^2)
     }
