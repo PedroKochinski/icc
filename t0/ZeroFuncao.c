2e25de807ea98a7e02fb702a71940925f6bc598e
pedrokochinski <pedrokochinski@gmail.com>
2025-04-04 14:56:56 -0300

    refactor: update ULP calculation and improve polynomial methods

--- t0/ZeroFuncao.c
+++ t0/ZeroFuncao.c
@@ -1,5 +1,5 @@
 #include "ZeroFuncao.h"
-
+#include "DoubleType.h"
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -7,22 +7,15 @@
 
 #include "utils.h"
 
-// Converte double para int64 preservando os bits
-int64_t double_to_int64(double d) {
-    int64_t i;
-    memcpy(&i, &d, sizeof(double));
-    // Ajuste para que números negativos fiquem ordenados corretamente
-    return (i < 0) ? INT64_MIN - i : i;
-}
-
-int64_t calcula_diferenca_ulp(double a, double b) {
-    // Se forem exatamente iguais, a diferença é zero
-    if (a == b) return 0;
-
-    int64_t ia = double_to_int64(a);
-    int64_t ib = double_to_int64(b);
-    int64_t diff = llabs(ia - ib);
-    return diff;
+long calcula_diferenca_ulp(real_t a, real_t b) {
+    Double_t A, B;
+    A.f = a;
+    B.f = b;
+    // Different signs means they do not match.
+    // Check for equality to make sure +0==-0
+    if ((A.parts.sign != B.parts.sign) && A.f == B.f) return 0;
+    long ulpsDiff = labs(A.i - B.i) - 1; // valor absoluto é UM + o número de floats representáveis entre eles
+    return ulpsDiff;
 }
 
 int verifica_parada(int it, int criterioParada, double x0, double raiz, double px) {
@@ -35,7 +28,7 @@ int verifica_parada(int it, int criterioParada, double x0, double raiz, double p
     if (criterioParada == 2 && fabs(px) <= DBL_EPSILON) {  // criterio 2 - se o valor da de p(x) é quae zero, i.e |p(x)| <= epsilon
         return 1;
     }
-    if (criterioParada == 3 && (it > 2) && calcula_diferenca_ulp(x0, raiz) <= ULPS) {  // criterio 3 - ULP's entre xk e xk-1 <= 2
+    if (criterioParada == 3 && (it > 2) && calcula_diferenca_ulp(raiz, x0) <= ULPS) {  // criterio 3 - ULP's entre xk e xk-1 <= 2
         return 1;
     }
     return 0;
@@ -69,7 +62,7 @@ real_t newtonRaphson(Polinomio p, real_t x0, int criterioParada, int *it, real_t
         (*it)++;
         x0 = *raiz; // recebe a ultima raiz candidata
         *raiz = *raiz - (px / derivada);  // calcula a nova raiz candidata
-        erro = fabs(*raiz - x0) / *raiz * 100; // calcula o erro relativo
+        erro = fabs(*raiz - x0 / *raiz) * 100; // calcula o erro relativo
 
         if (verifica_parada(*it, criterioParada, x0, *raiz, px)) {
             parada = 1;
@@ -92,9 +85,10 @@ real_t bisseccao(Polinomio p, real_t a, real_t b, int criterioParada, int *it, r
     int parada = 0;
     real_t px, calcA, calcB, erro = 0.0, ultimaRaiz;
     while (!parada) {
+        (*it)++;
         ultimaRaiz = *raiz;
         *raiz = (a + b) / 2;  // calcula a raiz candidata, o ponto medio do intervalo
-        erro = (fabs(ultimaRaiz - *raiz) / ultimaRaiz) * 100;
+        erro = fabs(*raiz - ultimaRaiz / *raiz) * 100;
         if (tipoCalculo == 0) {
             calcPolinomio_rapido(p, a, &calcA, NULL);
             calcPolinomio_rapido(p, b, &calcB, NULL);
@@ -104,13 +98,14 @@ real_t bisseccao(Polinomio p, real_t a, real_t b, int criterioParada, int *it, r
             calcPolinomio_lento(p, b, &calcB, NULL);
             calcPolinomio_lento(p, *raiz, &px, NULL);
         }
-        (*it)++;
+
 
         if (calcA * px < 0) {
             b = *raiz;  // B recebe raiz, pois a raiz está no intervalo [a, raiz]
         } else if (calcA * px > 0) {
             a = *raiz;  // A recebe raiz, pois a raiz está no intervalo [raiz, b]
-        } else {
+        } 
+        else {
             parada = 1;
             return erro;  // achou a raiz, retorna o erro
         }
@@ -120,7 +115,6 @@ real_t bisseccao(Polinomio p, real_t a, real_t b, int criterioParada, int *it, r
             return erro;  // achou a raiz ou iterações excedidas, retorna o erro
         }
     }
-
     return erro;
 }
 
@@ -151,14 +145,11 @@ real_t *dpx - valor da derivada de P(x) calculado, i.e, f'(x)
 void calcPolinomio_lento(Polinomio p, real_t x, real_t *px, real_t *dpx) {
     *px = 0;
     real_t calculo_dpx = 0;
-    for (int i = p.grau; i >= 0; i--) {
+    for (int i = p.grau; i > 0; i--) {
         *px += p.p[i] * pow(x, i);  // O(n^2)
+        /*calculo da derivada*/
+        calculo_dpx += p.p[i] * i * pow(x, i - 1);  // O(n^2), pode ter erro de calculo ( cancelamento subtravitivo, arredondamento, etc. )
     }
-    /*calculo da derivada*/
-    if (dpx != NULL) {
-        for (int i = p.grau; i > 0; i--) {
-            calculo_dpx += p.p[i] * i * pow(x, i - 1);  // O(n^2), pode ter erro de calculo ( cancelamento subtravitivo, arredondamento, etc. )
-        }
-        *dpx = calculo_dpx;
-    }
+    *px += p.p[0] * pow(x, 0);  // O(n^2)
+    if (dpx != NULL) *dpx = calculo_dpx;
 }
